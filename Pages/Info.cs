909bd42f7c5e5d38211699a4148a1ac53af5e04a
joshuadwalker11 <joshuadwalker11@gmail.com>
2023-04-24 08:33:11 -0400

    info_page

--- Pages/Info.cs
+++ Pages/Info.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Final_Project.Pages;
 
-public class PrivacyModel : PageModel
+public class InfoModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
+    private readonly ILogger<InfoModel> _logger;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public InfoModel(ILogger<InfoModel> logger)
     {
         _logger = logger;
     }
